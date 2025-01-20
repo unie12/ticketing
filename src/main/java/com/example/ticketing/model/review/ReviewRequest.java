1a92597db2c81b150b291196134574fa76a9a463
unie12 <joedaehui1@naver.com>
2025-01-20 16:01:59 +0900

    [BE]: 리뷰 이미지 + 방문 정보 추가 dto

--- src/main/java/com/example/ticketing/model/review/ReviewRequest.java
+++ src/main/java/com/example/ticketing/model/review/ReviewRequest.java
@@ -1,11 +1,20 @@
 package com.example.ticketing.model.review;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+import org.apache.catalina.LifecycleState;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
 public class ReviewRequest {
     private String content;
-    private int rating;
+    private Integer rating;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime visitDateTime;
+    private Crowdedness crowdedness;
+//    private List<String> imageUrls;
 }
--- src/main/java/com/example/ticketing/model/review/ReviewResponse.java
+++ src/main/java/com/example/ticketing/model/review/ReviewResponse.java
@@ -1,20 +1,23 @@
 package com.example.ticketing.model.review;
 
-import com.example.ticketing.model.store.Store;
-import com.example.ticketing.model.user.User;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
 public class ReviewResponse {
     private Long id;
     private String content;
-    private int rating;
+    private Integer rating;
     private String userName;
     private String storeName;
+    private LocalDateTime visitDateTime;
+    private Crowdedness crowdedness;
+    private List<String> imageUrls;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
@@ -25,6 +28,9 @@ public class ReviewResponse {
                 .rating(review.getRating())
                 .userName(review.getUser().getUsername())
                 .storeName(review.getStore().getPlaceName())
+                .visitDateTime(review.getVisitInfo() != null ? review.getVisitInfo().getVisitDateTime() : null)
+                .crowdedness(review.getVisitInfo() != null ? review.getVisitInfo().getCrowdedness() : null)
+                .imageUrls(review.getImages().stream().map(ReviewImage::getImageUrl).collect(Collectors.toList()))
                 .createdAt(review.getCreatedAt())
                 .updatedAt(review.getUpdatedAt())
                 .build();
